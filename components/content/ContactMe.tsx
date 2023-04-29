2f17ca91fd9da8bbf3aee848788eaf74cc866744
Marco <marcocpdti@gmail.com>
2023-04-29 16:41:16 -0300

    fix

--- components/content/ContactMe.tsx
+++ components/content/ContactMe.tsx
@@ -12,7 +12,7 @@ const ContactMe = () => {
 
         <div className='w-full flex justify-between items-center flex-col lg:flex-row gap-6 mb-10'>
 
-          <a href='mailto:jeanrdev@gmail.com' target='_blank' rel='noreferrer' className='w-full'>
+          <a href='mailto:marcocpdti@gmail.com' target='_blank' rel='noreferrer' className='w-full'>
             <div className='rounded border border-gray-800 hover:border-gray-900 bg-gray-900 p-4 shadow-sm hover:shadow-[0_8px_30px_rgb(0,0,0,0.12)] sm:p-6 transition ease'>
               <h6 className='font-bold text-1xl tracking-tight text-white text-start'>
                 Email
