017cf9a1324ae97c9f2ee8967584922a1b0cc0cf
Marco <marcocpdti@gmail.com>
2023-04-30 21:29:23 -0300

    retirando token api github

--- app/api/repositories/route.ts
+++ app/api/repositories/route.ts
@@ -1,10 +1,9 @@
 import { NextResponse } from 'next/server'
 
 export async function GET() {
-    const apikey = "Bearer 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"
+    // const apikey = "Bearer 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"
     const res = await fetch(
-        'https://api.github.com/user/repos?page=1&per_page=100',
-        { headers: { Authorization: apikey } }
+        'https://api.github.com/users/marcolongitude/repos?page1&per_page=100',
     )
 
     if (!res.ok) throw new Error(`HTTP error! Status: ${res.status}`);
