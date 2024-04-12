8a518b9c4f7952e5219fb4b42efd266900226f55
Marco Aurélio Guimarães <marcocpdti@gmail.com>
2024-04-12 18:50:26 -0300

    cache fetch

--- app/(user)/blog/page.tsx
+++ app/(user)/blog/page.tsx
@@ -1,42 +1,58 @@
-import CardPostsBlog from "@/components/content/CardPostsBlog";
-import SectionContainer from "@/components/utils/SectionContainer";
-import { use } from "react";
+import CardPostsBlog from '@/components/content/CardPostsBlog';
+import SectionContainer from '@/components/utils/SectionContainer';
+import { use } from 'react';
 import { DataPostsTabNews } from '../../../types/index';
 
-const baseUrl: string = "https://www.tabnews.com.br/marcocpdti/"
+const baseUrl: string = 'https://www.tabnews.com.br/marcocpdti/';
 
 const getData = async (): Promise<Array<DataPostsTabNews>> => {
-    const data = await fetch('https://www.tabnews.com.br/api/v1/contents/marcocpdti')
-    if (!data.ok) throw new Error(`HTTP error! Status: ${data.status}`)
-    const posts = await data.json()
+  const data = await fetch(
+    'https://www.tabnews.com.br/api/v1/contents/marcocpdti',
+    {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      cache: 'force-cache',
+      next: { tags: ['posts-tabnews'] }
+    }
+  );
+  if (!data.ok) throw new Error(`HTTP error! Status: ${data.status}`);
+  const posts = await data.json();
 
-    const response = posts.map((post: DataPostsTabNews) => {
-        return {
-            id: post.id,
-            title: post.title,
-            source_url: baseUrl + post.slug,
-            created_at: post.created_at,
-        }
-    });
+  const response = posts.map((post: DataPostsTabNews) => {
+    return {
+      id: post.id,
+      title: post.title,
+      source_url: baseUrl + post.slug,
+      created_at: post.created_at
+    };
+  });
 
-    return response
-}
+  return response;
+};
 
 const Blog = () => {
-    const allPosts: Array<DataPostsTabNews> = use(getData())
-    return (
-        <SectionContainer>
-            <div className='w-full flex flex-col items-center gap-6' style={{ height: '100vh' }}>
-
-                {allPosts && allPosts.map(({ id, title, source_url, created_at }) => (
-                    <div className='w-full' key={id}>
-                        <CardPostsBlog title={title} created_at={created_at} source_url={source_url} />
-                    </div>
-                ))}
-
+  const allPosts: Array<DataPostsTabNews> = use(getData());
+  return (
+    <SectionContainer>
+      <div
+        className="w-full flex flex-col items-center gap-6"
+        style={{ height: '100vh' }}
+      >
+        {allPosts &&
+          allPosts.map(({ id, title, source_url, created_at }) => (
+            <div className="w-full" key={id}>
+              <CardPostsBlog
+                title={title}
+                created_at={created_at}
+                source_url={source_url}
+              />
             </div>
-        </SectionContainer>
-    )
+          ))}
+      </div>
+    </SectionContainer>
+  );
 };
 
 export default Blog;
--- components/content/SearchAllProjects.tsx
+++ components/content/SearchAllProjects.tsx
@@ -2,56 +2,74 @@ import { use } from 'react';
 import CardProject from './CardProject';
 
 type Propstype = {
-    id: string
-    name: string
-    description: string
-    language: string
-    svn_url: string
-    owner: {
-        login?: string
-        html_url: string
-    }
-}
+  id: string;
+  name: string;
+  description: string;
+  language: string;
+  svn_url: string;
+  owner: {
+    login?: string;
+    html_url: string;
+  };
+};
 
 const getData = async (): Promise<any> => {
-    let arrayPrepared: Propstype[] = []
+  let arrayPrepared: Propstype[] = [];
 
-    const data = await fetch('https://api.github.com/users/marcolongitude/repos?page1&per_page=100')
-    if (!data.ok) throw new Error(`HTTP error! Status: ${data.status}`)
-    const repositories = await data.json()
-
-    if (typeof repositories === 'object' && repositories.length > 0) {
-        repositories.forEach((repo: Propstype) => {
-            if (repo && repo?.owner?.login == 'marcolongitude') {
-                arrayPrepared.push({
-                    id: repo.id,
-                    name: repo.name,
-                    description: repo.description,
-                    language: repo.language,
-                    svn_url: repo.svn_url,
-                    owner: { html_url: repo.owner.html_url }
-                })
-            }
-        });
+  const data = await fetch(
+    'https://api.github.com/users/marcolongitude/repos?page1&per_page=100',
+    {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      cache: 'force-cache',
+      next: { tags: ['repositories-github'] }
     }
+  );
+  if (!data.ok) throw new Error(`HTTP error! Status: ${data.status}`);
+  const repositories = await data.json();
 
-    return arrayPrepared
-}
+  if (typeof repositories === 'object' && repositories.length > 0) {
+    repositories.forEach((repo: Propstype) => {
+      if (repo && repo?.owner?.login == 'marcolongitude') {
+        arrayPrepared.push({
+          id: repo.id,
+          name: repo.name,
+          description: repo.description,
+          language: repo.language,
+          svn_url: repo.svn_url,
+          owner: { html_url: repo.owner.html_url }
+        });
+      }
+    });
+  }
 
+  return arrayPrepared;
+};
 
 const SearchAllProjects = () => {
-    const allProjectsInfo: Propstype[] = use(getData())
+  const allProjectsInfo: Propstype[] = use(getData());
 
-    return (
-        <>
-            <article className='w-full flex justify-center items-center content-center flex-wrap gap-6 mx-auto'>
-                {
-                    allProjectsInfo && allProjectsInfo.map(({ id, name, description, language, svn_url, owner }) =>
-                        <CardProject key={id} title={name} des={description} category={language} repo={svn_url} link={owner.html_url} />)
-                }
-            </article>
-        </>
-    )
-}
+  return (
+    <>
+      <article className="w-full flex justify-center items-center content-center flex-wrap gap-6 mx-auto">
+        {allProjectsInfo &&
+          allProjectsInfo.map(
+            ({ id, name, description, language, svn_url, owner }) => (
+              <CardProject
+                key={id}
+                title={name}
+                des={description}
+                category={language}
+                repo={svn_url}
+                link={owner.html_url}
+              />
+            )
+          )}
+      </article>
+    </>
+  );
+};
 
 export default SearchAllProjects;
